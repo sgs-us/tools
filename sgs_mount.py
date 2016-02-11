bd659681ba935513bf18110a05782bdae254d2b8
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2016-02-11 08:34:31 +0100

    Adapt to the directory name unification.

--- sgs_mount.py
+++ sgs_mount.py
@@ -9,7 +9,7 @@ whoami = os.getlogin()
 mounts = [
     ["helium:/import/sgs.local", "~/mnt/sgs"],
     ["helium:", "~/mnt/home"],
-    ["helium:/data2/scratch/{}".format(whoami), "~/mnt/scratch"],
+    ["helium:/data/scratch/{}".format(whoami), "~/mnt/scratch"],
     ["neon:/data/scratch/{}".format(whoami), "~/mnt/neon_scratch"],
     ["ipvslogin:/import/www.ipvs", "~/mnt/www.ipvs"],
     # ["supermuc:", "~/mnt/supermuc"] 
